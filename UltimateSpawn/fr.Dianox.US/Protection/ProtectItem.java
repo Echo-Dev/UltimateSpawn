a0eacce762b1154a995a871e75aaa897939c1b3a
Echo Dev <dklinc300@gmail.com>
2017-04-15 14:50:48 +0200

    Missing import
    Fixed missint Event handeler import!

--- UltimateSpawn/fr.Dianox.US/Protection/ProtectItem.java
+++ UltimateSpawn/fr.Dianox.US/Protection/ProtectItem.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.EventHandler;
 
 import fr.Dianox.US.config.MainConfig;
 
